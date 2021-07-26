f4d498e2c7b3614dfd647e1614bdf25036f7148d
Yichun Zhao <zhaoyichun@hotmail.com>
2021-07-26 14:20:35 +0200

    bug:UserDetailsService when user not found throw AuthenticationException

--- src/main/java/com/ynz/demo/springsecurityjwt/AppUserDetailService.java
+++ src/main/java/com/ynz/demo/springsecurityjwt/AppUserDetailService.java
@@ -11,14 +11,16 @@ import java.util.Map;
 
 @Service
 public class AppUserDetailService implements UserDetailsService {
-    private Map<String, UserDetails> stringUserDetailsMap = new HashMap<>();
+    private final Map<String, UserDetails> stringUserDetailsMap = new HashMap<>();
 
     {
-        stringUserDetailsMap.put("foo", User.builder().username("foo").password("foo").authorities("USER").build());
+        stringUserDetailsMap.put("foo", User.builder().username("foo").password("foo").authorities("ROLE_USER").build());
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (stringUserDetailsMap.get(username) == null)
+            throw new UsernameNotFoundException(username + " is not found!");
         return stringUserDetailsMap.get(username);
     }
 
