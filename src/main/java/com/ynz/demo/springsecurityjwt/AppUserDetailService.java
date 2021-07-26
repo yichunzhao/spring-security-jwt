a53c4305d8cb720546370ec093317a5f2eb5fcaf
Yichun Zhao <zhaoyichun@hotmail.com>
2021-07-26 13:39:58 +0200

    bug:UserDetailsService constantly return the same UserDetails

--- src/main/java/com/ynz/demo/springsecurityjwt/AppUserDetailService.java
+++ src/main/java/com/ynz/demo/springsecurityjwt/AppUserDetailService.java
@@ -6,12 +6,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class AppUserDetailService implements UserDetailsService {
+    private Map<String, UserDetails> stringUserDetailsMap = new HashMap<>();
+
+    {
+        stringUserDetailsMap.put("foo", User.builder().username("foo").password("foo").authorities("USER").build());
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return User.builder().username("foo").password("foo").authorities("USER").build();
+        return stringUserDetailsMap.get(username);
     }
 
 }
